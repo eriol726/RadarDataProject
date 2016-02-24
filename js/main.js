6362b96046306b1ddf9e225ef69052ba920b3430
Erik Olsson <eriol726@student.liu.se>
2016-02-24 08:38:09 +0100

    better scale

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 
 var map1;
 
+var none = 1;
+    map1 = new map(none);
+/*
 d3.csv("data/data.csv", function (data) {
 
-    map1 = new map(data);
+	map1 = new map(none);
 
 });
 
+*/
--- js/map.js
+++ js/map.js
@@ -7,21 +7,29 @@ function map(data) {
     var mapDiv = $("#map");
 
     var margin = {top: 20, right: 20, bottom: 20, left: 20},
-    width = mapDiv.width() - margin.right - margin.left,
-            height = mapDiv.height() - margin.top - margin.bottom;
+        width = mapDiv.width() - margin.right - margin.left,
+        height = mapDiv.height() - margin.top - margin.bottom;
 
     var clustered = false;
 
     var format = d3.time.format.utc("%Y-%m-%dT%H:%M:%S.%LZ");
 
-    var timeExt = d3.extent(data.map(function (d) {
-        return format.parse(d.time);
-    }));
 
-    var filterdData = data;
+    //Sets the map projection
+    var projection = d3.geo.mercator()
+        .center([20, 55 ])
+        .scale(800);
 
-    //Sets the colormap
-    var colors = colorbrewer.Set3[10];
+    
+
+
+            //.center([8.25, 56.8])
+            //.scale(700);
+
+    //Creates a new geographic path generator and assing the projection        
+    var path = d3.geo.path()
+                .projection(projection);
+
 
     //Assings the svg canvas to the map div
     var svg = d3.select("#map").append("svg")
@@ -30,20 +38,17 @@ function map(data) {
             .call(zoom);
 
     var g = svg.append("g");
-
-    //Sets the map projection
-    var projection = d3.geo.mercator()
-            .center([8.25, 56.8])
-            .scale(700);
-
-    //Creates a new geographic path generator and assing the projection        
-    var path = d3.geo.path().projection(projection);
-
+    
     //console.log("data", geoData.features[0]);
     //Loads geo data
-    d3.json("data/world-topo.json", function (error, world) {
-        var countries = topojson.feature(world, world.objects.countries).features;
-        draw(countries);
+    d3.json("data/uk.json", function (error, uk) {
+        if (error) return console.error(error);
+        console.log("test", data);
+        svg.selectAll(".subunits")
+          .datum(topojson.feature(uk, uk.objects.subunits))
+          .enter().append("path")
+          .attr("d", path)
+          
     });
 
     //Calls the filtering function 
@@ -56,6 +61,7 @@ function map(data) {
     //Draws the map and the points
     function draw(countries)
     {
+
         //draw map
         var country = g.selectAll(".country").data(countries)
                     .enter().insert("path")
@@ -84,7 +90,7 @@ function map(data) {
         var s = d3.event.scale;
 
         zoom.translate(t);
-        g.style("stroke-width", 1 / s).attr("transform", "translate(" + t + ")scale(" + s + ")");
+        svg.style("stroke-width", 1 / s).attr("transform", "translate(" + t + ")scale(" + s + ")");
     }
 
     //Prints features attributes
