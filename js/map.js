913b1542d8fc07a15b75afa196f6f5cd23c37509
pelse862 <pelse862@student.liu.se>
2016-03-08 18:24:39 +0100

    merge

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 function map(data) {
     var self = this;
 
-    const LINES = 600;
+    const LINES = data.length;
     //Set threshold for circle radius depending on number of ids (LARGE, LARGER, LARGEST)
     const LARGE = 500, LARGER = 1500, LARGEST = 15000;
 
@@ -24,7 +24,7 @@ function map(data) {
         area1.update2(TotalRidesPerDay);
         self.marked = false;
         if(! (typeof self.flightPath == "undefined")){removeLine();}
-        d3.selectAll("circle").style("opacity",1)
+        d3.selectAll("circle").style("opacity",1).style("fill","orange")
         document.getElementById("graphRubrik").innerHTML="<h2>Total Customers per day</h2>"
 
 
